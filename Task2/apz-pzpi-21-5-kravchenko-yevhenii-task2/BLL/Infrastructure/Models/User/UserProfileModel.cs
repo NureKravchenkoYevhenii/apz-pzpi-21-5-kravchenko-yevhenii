6ef365d7e8f7bac260d3bf6a5fa3a5d6f59a5d61
Yevhenii Kravchenko <yevhenii.kravchenko@nure.ua>
2024-06-01 14:57:17 +0300

    Implemented backend

---
+++ Task2/apz-pzpi-21-5-kravchenko-yevhenii-task2/BLL/Infrastructure/Models/User/UserProfileModel.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Enums;
+
+namespace BLL.Infrastructure.Models;
+public class UserProfileModel
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public byte[]? ProfilePicture { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public DateTime BirthDate { get; set; }
+
+    public string BirthDateString { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public DateTime RegistrationDate { get; set; }
+
+    public Role Role { get; set; }
+}
